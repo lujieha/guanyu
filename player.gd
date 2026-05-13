e3ff18507abab07be0bafabbc6eb8b00f883f614
lujieh <lujieh@gmail.com>
2026-05-13 23:28:18 +0800

    更新

--- player.gd
+++ player.gd
@@ -6,21 +6,31 @@ extends CharacterBody2D
 
 
 
+@export var walk_speed: float = 100.0      # 移动速度
+@export var jump_velocity: float = -300.0  # 跳跃初速度（负值向上）
+@export var gravity: float = 400.0          # 重力加速度
+
 func _physics_process(delta: float) -> void:
-	var x_input = Input.get_axis("ui_left","ui_right")
-	velocity.x = x_input *100
-	#
+	# 水平移动
+	var x_input = Input.get_axis("ui_left", "ui_right")
+	velocity.x = x_input * walk_speed
 	
+	# 跳跃输入
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	# 重力（只在空中时生效）
 	if not is_on_floor():
-		velocity.y += 80 * delta
+		velocity.y += gravity * delta
+		animation_player.play("jump")
 	
-	if x_input==0:
-		animation_player.play("stand")
-	else:	
-		print(sign(x_input))
-		animation_player.play("run")
-		player_body.scale.x= sign(x_input)
-		
-		
+	# 动画处理
+	if x_input == 0:
+		if is_on_floor():  # 只有在地面才播放站立
+			animation_player.play("stand")
+	else:
+		if is_on_floor():  # 只有在地面才播放跑步
+			animation_player.play("run")
+		player_body.scale.x = sign(x_input)
 	
 	move_and_slide()
